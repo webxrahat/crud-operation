c5fe3137c86a3d8c83694e254b06be6e839f2d3d
webxrahat <islammdrahat308@gmail.com>
2024-10-14 09:45:57 +0600

    rr

---
+++ testForm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form action="">
+
+            <input type="text" name="name"><br><br>
+            <input type="text" name="age"><br><br>
+            <input type="submit" value="submit"><br><br>
+
+        </form>
+    </div>
+</body>
+
+</html>
